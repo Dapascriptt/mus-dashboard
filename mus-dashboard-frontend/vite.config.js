325fcb77c958eac00f47b3ca5f8a24649db33556
Dapascriptt <136902837+Dapascriptt@users.noreply.github.com>
2025-11-28 15:30:44 +0800

    Update

--- mus-dashboard-frontend/vite.config.js
+++ mus-dashboard-frontend/vite.config.js
@@ -1,9 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer({
+      filename: 'stats.html',
+      template: 'treemap', // atau 'sunburst'
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   server: {
     proxy: {
       // Memastikan panggilan ke "/api" di dev diarahkan ke backend lokal
